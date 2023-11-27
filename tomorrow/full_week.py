1967813f20892da3bbd9c8dd341174b85026cb11
mikeyesteban <michaelp.esteban@gmail.com>
2023-11-27 07:41:20 -0500

    fixed selector

--- tomorrow/full_week.py
+++ tomorrow/full_week.py
@@ -11,7 +11,8 @@ def run_full_week_csv(browser, tab_id, studio_name, date_format):
 
     browser.implicitly_wait(.3)
     flag, counter = False, 0
-    all_rows = browser.find_element(By.ID, "classSchedule-mainTable").find_elements(By.TAG_NAME, 'tr')
+    main_table = browser.find_element(By.ID, "classSchedule-mainTable")
+    all_rows = main_table.find_elements(By.XPATH, "//*[@class='evenRow row' or @class='header' or  @class='oddRow row']")
     correct_date_rows = []
 
     for row in all_rows:
