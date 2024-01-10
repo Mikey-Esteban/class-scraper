59caa3f4912c764a9bfa8416a4ea362729b20cde
Ajay Rijhsinghani <ajayrijh@Ajays-MBP.lan>
2024-01-10 00:33:56 -0500

    paused pjm and gramercy due to errors

--- today/script.sh
+++ today/script.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 python3 mind_body.py
-python3 gramercy.py
+# python3 gramercy.py
 python3 modega.py
-python3 pjm.py
+# python3 pjm.py
 python3 create_social_csv.py
 python3 categorize.py
 python3 csv_to_gs.py
